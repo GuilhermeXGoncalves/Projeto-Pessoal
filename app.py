45951c5e33a3da80c6a75e94ee782f30bab66715
Guilherme Gonçalves <112209360+GuilhermeXGoncalves@users.noreply.github.com>
2024-02-07 17:14:17 -0300

    Criando a API e recebendo dados os dados do form

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
-from Flask import Flask
+from flask import Flask, render_template, redirect, request
 
-print('Olá')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'TESTE'
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+    print(nome)
+    print(senha)
+    return redirect('/')
+
+if __name__ in "__main__":
+    app.run(debug=True)
