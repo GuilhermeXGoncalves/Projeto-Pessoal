2d1ea0bccc6c323daa7ba229aa5d274344145829
Guilherme Gonçalves <112209360+GuilhermeXGoncalves@users.noreply.github.com>
2024-02-08 18:54:37 -0300

    Implementando a parte de cadastro

--- app.py
+++ app.py
@@ -3,17 +3,33 @@ from flask import Flask, render_template, redirect, request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'TESTE'
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def home():
     return render_template('login.html')
 
-@app.route('/login', methods=['POST'])
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     nome = request.form.get('nome')
     senha = request.form.get('senha')
     print(nome)
     print(senha)
     return redirect('/')
+
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
+    email = request.form.get('email')
+
+    print(nome)
+    print(senha)
+    print(email)
+
+    return render_template('cadastro.html')
+
 
 if __name__ in "__main__":
     app.run(debug=True)
